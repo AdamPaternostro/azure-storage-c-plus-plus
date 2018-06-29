9be0237b0270177a4bef24693374dbfca66aba13
Adam Paternostro <AdamPaternostro@users.noreply.github.com>
2018-06-29 14:49:47 -0400

    Add files via upload

---
+++ Docker-Multi-Stage-Build/run.sh
@@ -0,0 +1,3 @@
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/tmp/azure-storage-cpp/Microsoft.WindowsAzure.Storage/build.release/Binaries
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/tmp/casablanca/Release/build.release/Binaries 
+./sample 
